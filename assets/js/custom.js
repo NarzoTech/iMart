c9b412d715271a643fb5e032c82fe7a8e1e6a34d
sazalahmed <sazal.web@gmail.com>
2025-01-24 22:09:24 +0600

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,77 @@
+$(function () {
+
+    "use strict";
+
+
+    //======MENU FIX JS=======   
+    var navoff = $('.main_menu').offset().top;
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+
+        if (scrolling > navoff) {
+            $('.main_menu').addClass('menu_fix');
+        } else {
+            $('.main_menu').removeClass('menu_fix');
+        }
+    });
+
+
+    //======HESDER FIX JS=======   
+    $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        if (scrolling > navoff) {
+            $('header').addClass('header_fix');
+        } else {
+            $('header').removeClass('header_fix');
+        }
+    });
+
+
+    //=====CATEGORY MENU======  
+    $('.menu_category_bar').on('click', function () {
+        $('.toggle_menu').toggleClass('show_category');
+    });
+
+    $('.menu_category_bar').on('click', function () {
+        $('.menu_category_bar').toggleClass('ratate_arrow');
+    });
+
+
+    //===venobox js===
+    $('.venobox').venobox();
+
+
+    //=======Simply Countdown======   
+    var d = new Date(),
+        countUpDate = new Date();
+    d.setDate(d.getDate() + 365);
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: true
+    });
+
+
+    //======countUp js=========   
+    $('.counter').countUp();
+
+
+    //=======SELECT2======== 
+    $(document).ready(function () {
+        $('.select_2').select2();
+    });
+
+
+    //======NICE SELECT=======
+    $('.select_js').niceSelect();
+
+
+    //=====WOW JS====== 
+    new WOW().init();
+
+
+
+
+
+});
