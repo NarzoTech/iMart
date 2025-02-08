71d641f65e2ba15e69e4df09077e4be0de633c82
sazalahmed <sazal.web@gmail.com>
2025-02-09 02:22:41 +0600

    home 2 started

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -123,6 +123,16 @@ $(function () {
         defaultTab: 1,
     });
 
+    //=====BANNER SLIDER===== 
+    $('.banner_2_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+        fade: true,
+    });
 
 
 });
