7bdea02365ac8818e7b19168c92320ad75cf049f
sazalahmed <sazal.web@gmail.com>
2025-03-06 04:23:23 +0600

    about page added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -466,4 +466,44 @@ $(function () {
         ]
     });
 
+
+    //=====TESTIMONIAL 2 SLIDER====== 
+    $('.testi_slider_2').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-angle-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-angle-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
 });
